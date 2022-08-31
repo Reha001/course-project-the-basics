e7f1dbb7be07e39b5394a6a97d10cd3a5a667a65
Reha Ok <rehaok@Rehas-Mac-mini.local>
2022-08-31 16:31:37 +0300

    part 6 done with a bug

--- src/app/recipies/recipe-list/recipe-list.component.ts
+++ src/app/recipies/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { ThisReceiver } from '@angular/compiler';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
@@ -6,11 +7,20 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('A test Recipe', 'This is simply a test', 'imagepath here'),
-    new Recipe('A test Recipe', 'This is simply a test', 'imagepath here'),
+    new Recipe(
+      'Another test Recipe',
+      'This is simply a test',
+      'imagepath here'
+    ),
   ];
   constructor() {}
 
   ngOnInit() {}
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
 }
--- src/app/recipies/recipies.component.ts
+++ src/app/recipies/recipies.component.ts
@@ -1,15 +1,14 @@
+import { Recipe } from './recipe.model';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-recipies',
   templateUrl: './recipies.component.html',
-  styleUrls: ['./recipies.component.css']
+  styleUrls: ['./recipies.component.css'],
 })
 export class RecipiesComponent implements OnInit {
+  selectedRecipe: Recipe;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
